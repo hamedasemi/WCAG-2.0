00ee813c101f4e5d3d17b3ae62142b3b23253861
hamedasemi <h-@live.se>
2017-09-13 13:59:02 +0200

    Multilang works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -8,12 +8,26 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
   title = 'app';
+
+  @ViewChild('article') article;
+
   constructor(public translate: TranslateService) {
     this.translate.addLangs(['en', 'sv']);
     this.translate.setDefaultLang('en');
   }
-  changeLang(lang: string) {
-    console.log(this.translate);
-    this.translate.use(lang);
+
+  changeLang(event) {
+    console.log(event.target.value);
+    this.translate.use(event.target.value);
+  }
+
+  search(event) {
+    let search = [].slice.call(this.article.nativeElement.querySelectorAll('section')).filter((element) => {
+      if (!event.target.value.length || element.innerHTML.search(event.target.value) > 0) {
+        element.removeAttribute('hidden');
+      } else {
+        element.setAttribute('hidden', true);
+      }
+    });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -44,3 +44,4 @@ export function translate (http: HttpClient) {
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
