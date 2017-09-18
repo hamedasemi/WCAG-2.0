dfb5ddaa28e3fc5e755deea3f9ac1d1e27868bae
hamedasemi <h-@live.se>
2017-09-18 20:51:03 +0200

    clean

--- src/app/shared/category-filter.pipe.ts
+++ src/app/shared/category-filter.pipe.ts
@@ -7,7 +7,7 @@ export class CategoryFilterPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
     console.log(value)
-    return args ? value.filter((item: any)=>{
+    return args ? value.filter((item: any) => {
       return item.category === args
     }) : value;
   }
